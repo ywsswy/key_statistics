46054472db8e604c26cddee3fc0440edc0e250dd
hill <ql1119079560@yahoo.com>
2020-02-16 22:20:29 +0800

    fix caton

--- main.py
+++ main.py
@@ -2,11 +2,24 @@
 
 import pynput
 import os
+import threading
+import time
+
 class YGlobal(object):
     mouse_controller_ = pynput.mouse.Controller()
     key_map = {}
     count = 0
-    redraw = 10
+    flag = True
+
+def ReDraw():
+    YGlobal.flag = False
+    res = sorted(YGlobal.key_map.items(),key = lambda x:x[1],reverse = True)
+    os.system("cls")
+    print('total: {} times.'.format(YGlobal.count))
+    for i in res[:50]:
+        print('{percent:>5.2f}%{times:>6}times:\t\t{key}'.format(percent = i[1]*100/YGlobal.count, times = i[1], key = i[0]))
+    time.sleep(0.1)
+    YGlobal.flag = True
 
 def OnRelease(key):
     key = '{}'.format(key)
@@ -15,12 +28,8 @@ def OnRelease(key):
     else:
         YGlobal.key_map[key] = 1
     YGlobal.count = YGlobal.count + 1
-    if YGlobal.count % YGlobal.redraw == 0:
-        res = sorted(YGlobal.key_map.items(),key = lambda x:x[1],reverse = True)
-        os.system("cls")
-        print('total: {} times.'.format(YGlobal.count))
-        for i in res[:25]:
-            print('{percent:>5.2f}%{times:>6}times:\t\t{key}'.format(percent = i[1]*100/YGlobal.count, times = i[1], key = i[0]))
+    if YGlobal.flag:
+        threading.Timer(0, ReDraw).start()
 
 with pynput.keyboard.Listener(on_release = OnRelease) as listener:
     listener.join()
